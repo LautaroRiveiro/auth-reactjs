89521b308bbdb07b842ab4fa49b2497133e13a21
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-09 13:08:25 -0300

    Rutas: Refactor del BrowseRouter como HOC de index

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { HomePage } from "../pages/HomePage"
 import { LoginPage } from "../pages/LoginPage"
 import { NotFoundPage } from "../pages/NotFoundPage"
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
