eb8bc1a72e42203f610ba22878b2c30ffd1114aa
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-09 17:57:11 -0300

    Rutas: Redirect a la ruta original luego del login

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,14 +1,15 @@
-import { Navigate } from "react-router-dom"
+import { Navigate, useLocation } from "react-router-dom"
 import { useAuth } from "../auth/useAuth"
 import { ForbiddenPage } from "../pages/ForbiddenPage"
 
 export const PrivateRoute = ({ hasRole: role, children }) => {
 
+  const location = useLocation()
   const auth = useAuth()
 
   if (role && auth) {
     return auth.role === role ? children : <ForbiddenPage />
   }
 
-  return auth ? children : <Navigate to="/login" />
+  return auth ? children : <Navigate to="/login" state={{ from: location }} />
 }
--- src/router/PublicRoute.jsx
+++ src/router/PublicRoute.jsx
@@ -1,9 +1,9 @@
-import { Navigate } from "react-router-dom"
+import { Navigate, useLocation } from "react-router-dom"
 import { useAuth } from "../auth/useAuth"
 
 export const PublicRoute = ({ children }) => {
-
+  const location = useLocation()
   const auth = useAuth()
 
-  return auth ? <Navigate to="/" /> : children
+  return auth ? <Navigate to={location.state.from || "/"} /> : children
 }
